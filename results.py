3c32d0b5106da3bc90dda4e11ef7967a9a297c3c
Oliver Ebenhoeh <oliver.ebenhoeh@hhu.de>
2015-12-14 19:36:42 +0100

    Here are the common result analysis routines, such as 'getT', 'getV', etc.

---
+++ results.py
@@ -0,0 +1,61 @@
+__author__ = 'oliver'
+
+import numpy as np
+
+
+class Results():
+
+    def __init__(self, s):
+        """
+        class of methods inheriting from Simulation useful for plotting and analysis
+        """
+        self.model = s.model
+        self.results = s.results
+
+
+    def getT(self, r=None):
+        """
+        :param r: range of steps of simulation for which results we are interested in
+        :return: time of all results as one vector
+        """
+        if r is None:
+            r = range(len(self.results))
+
+        T = np.hstack([self.results[i]['t'] for i in r])
+
+        return T
+
+    def getVar(self, j, r=None):
+        """
+        :param j: int of the state variable [0:PQred, ..., 5:ATPsynth]
+        :param r: range of steps of simulation for which results we are interested in
+        :return: concentrations of variable j as one vector
+        """
+        if r is None:
+            r = range(len(self.results))
+
+        X = np.hstack([self.results[i]['y'][:, j] for i in r])
+
+        return X
+
+    def getRate(self, rate, r=None):
+        """
+        :param rate: name of the rate
+        :param r: range of steps of simulation for which results we are interested in
+        :return: rate with name 'rate' of all results as one vector
+        """
+        if r is None:
+            r = range(len(self.results))
+
+        V = np.array([])
+
+        for i in r:
+            t = self.results[i]['t']
+            y = self.results[i]['y']
+
+            V = np.hstack([V,
+                           np.array(
+                           [self.model.rates(y[j])[rate] for j in range(len(t))]
+                           )])
+
+        return V
