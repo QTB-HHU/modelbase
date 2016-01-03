755521f8afed653f2ec67faa700fba95d3da65c2
Oliver Ebenhoeh <oliver.ebenhoeh@hhu.de>
2016-01-03 11:48:18 +0100

    method timeCourse only reports when warnings are on

--- simulate.py
+++ simulate.py
@@ -76,17 +76,17 @@ class Simulate:
         self._successful = True
 
         Y = [y0]
-        print Y, type(Y)
+        #print Y, type(Y)
         cnt = 1
         while cnt < len(T) and self.successful():
             if self._warnings:
                 print cnt, Y
+                print(T[cnt])
             Y.append(self.integrate(T[cnt], Y[cnt-1], t0=T[cnt-1],
                                     minstep=minstep,
                                     maxstep=maxstep,
                                     nsteps=nsteps,
                                     integrator=integrator))
-            print(T[cnt])
             cnt += 1
 
         if self.doesMonitor() and self.successful():
