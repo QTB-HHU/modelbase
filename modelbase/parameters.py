c482f4b129397dcc84c3ef9d479371f483a61348
Marvin van Aalst <marvin.vanaalst@gmail.com>
2018-06-07 13:21:49 +0200

    Allowed ParameterSet input as update method for parameter update and inserted warning when overwriting parameters with ParameterSet.update()

--- modelbase/parameters.py
+++ modelbase/parameters.py
@@ -9,13 +9,13 @@ Created on Tue Mar  3 22:19:33 2015
 # TODO: allow dictionary or ParameterSet as pars / defaultpars
 
 class ParameterSet(object):
-    
+
     def __init__(self, pars={}, defaultpars={}):  # -- Anna changed here for pars to be optional
-        ''' 
-        sets parameters to defaultpars, 
+        '''
+        sets parameters to defaultpars,
         overwrites these with pars if provided
         '''
-        
+
         mypars = pars.copy()
 
         for k in defaultpars.keys():
@@ -28,9 +28,18 @@ class ParameterSet(object):
 
     def update(self, pars):
         '''
-        updates parameters with keys/values provided in pars
+        Updates parameters
+        Input:
+            pars: dictionary or ParameterSet object
         '''
 
-        for k,v in pars.items():
-            setattr(self,k,v)
-
+        if isinstance(pars,dict):
+            replaced_keys = [key for key in self.__dict__.keys() if key in pars]
+            if replaced_keys:
+                print("Warning: overwriting keys",replaced_keys)
+            for k,v in pars.items():
+                setattr(self,k,v)
+        elif isinstance(pars,ParameterSet):
+            self.update(pars.__dict__)
+        else:
+            raise TypeError("Function requires dict or ParameterSet input")
