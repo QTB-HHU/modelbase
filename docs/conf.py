0c58a690335307aca8178458a5556ec92cc1fd2b
Van Aalst, Marvin Christian (mavan106) <marvin.van.aalst@uni-duesseldorf.de>
2018-08-15 22:15:18 +0200

    Updated docs

---
+++ docs/conf.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, unicode_literals
+
+from datetime import datetime
+
+from recommonmark.parser import CommonMarkParser
+
+extensions = ['sphinx.ext.autodoc']
+templates_path = ['/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx', 'templates', '_templates', '.templates']
+source_suffix = ['.rst', '.md']
+source_parsers = {
+            '.md': CommonMarkParser,
+        }
+master_doc = 'index'
+project = u'modelbase'
+copyright = str(datetime.now().year)
+version = 'latest'
+release = 'latest'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+htmlhelp_basename = 'modelbase'
+html_theme = 'sphinx_rtd_theme'
+file_insertion_enabled = False
+latex_documents = [
+  ('index', 'modelbase.tex', u'modelbase Documentation',
+   u'', 'manual'),
+]
+
+
+
+
+###########################################################################
+#          auto-created readthedocs.org specific configuration            #
+###########################################################################
+
+
+#
+# The following code was added during an automated build on readthedocs.org
+# It is auto created and injected for every build. The result is based on the
+# conf.py.tmpl file found in the readthedocs.org codebase:
+# https://github.com/rtfd/readthedocs.org/blob/master/readthedocs/doc_builder/templates/doc_builder/conf.py.tmpl
+#
+
+
+import sys
+import os.path
+from six import string_types
+
+from sphinx import version_info
+
+# Get suffix for proper linking to GitHub
+# This is deprecated in Sphinx 1.3+,
+# as each page can have its own suffix
+if globals().get('source_suffix', False):
+    if isinstance(source_suffix, string_types):
+        SUFFIX = source_suffix
+    else:
+        SUFFIX = source_suffix[0]
+else:
+    SUFFIX = '.rst'
+
+# Add RTD Static Path. Add to the end because it overwrites previous files.
+if not 'html_static_path' in globals():
+    html_static_path = []
+if os.path.exists('_static'):
+    html_static_path.append('_static')
+html_static_path.append('/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx/_static')
+
+# Add RTD Theme only if they aren't overriding it already
+using_rtd_theme = False
+if 'html_theme' in globals():
+    if html_theme in ['default']:
+        # Allow people to bail with a hack of having an html_style
+        if not 'html_style' in globals():
+            import sphinx_rtd_theme
+            html_theme = 'sphinx_rtd_theme'
+            html_style = None
+            html_theme_options = {}
+            if 'html_theme_path' in globals():
+                html_theme_path.append(sphinx_rtd_theme.get_html_theme_path())
+            else:
+                html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+            using_rtd_theme = True
+else:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_style = None
+    html_theme_options = {}
+    if 'html_theme_path' in globals():
+        html_theme_path.append(sphinx_rtd_theme.get_html_theme_path())
+    else:
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    using_rtd_theme = True
+
+if globals().get('websupport2_base_url', False):
+    websupport2_base_url = 'https://readthedocs.org/websupport'
+    if 'http' not in settings.MEDIA_URL:
+        websupport2_static_url = 'https://media.readthedocs.org/static/'
+    else:
+        websupport2_static_url = 'https://media.readthedocs.org//static'
+
+
+#Add project information to the template context.
+context = {
+    'using_theme': using_rtd_theme,
+    'html_theme': html_theme,
+    'current_version': "latest",
+    'version_slug': "latest",
+    'MEDIA_URL': "https://media.readthedocs.org/",
+    'PRODUCTION_DOMAIN': "readthedocs.org",
+    'versions': [
+    ("latest", "/en/latest/"),
+    ],
+    'downloads': [ 
+    ("pdf", "//readthedocs.org/projects/modelbase/downloads/pdf/latest/"),
+    ("htmlzip", "//readthedocs.org/projects/modelbase/downloads/htmlzip/latest/"),
+    ("epub", "//readthedocs.org/projects/modelbase/downloads/epub/latest/"),
+    ],
+    'subprojects': [ 
+    ],
+    'slug': 'modelbase',
+    'name': u'modelbase',
+    'rtd_language': u'en',
+    'programming_language': u'py',
+    'canonical_url': 'https://modelbase.readthedocs.io/en/latest/',
+    'analytics_code': '',
+    'single_version': False,
+    'conf_py_path': '/docs/',
+    'api_host': 'https://readthedocs.org',
+    'github_user': 'None',
+    'github_repo': 'None',
+    'github_version': 'master',
+    'display_github': False,
+    'bitbucket_user': 'None',
+    'bitbucket_repo': 'None',
+    'bitbucket_version': 'master',
+    'display_bitbucket': False,
+    'gitlab_user': 'ebenhoeh',
+    'gitlab_repo': 'modelbase',
+    'gitlab_version': 'master',
+    'display_gitlab': True,
+    'READTHEDOCS': True,
+    'using_theme': (html_theme == "default"),
+    'new_theme': (html_theme == "sphinx_rtd_theme"),
+    'source_suffix': SUFFIX,
+    'ad_free': False,
+    'user_analytics_code': '',
+    'global_analytics_code': 'UA-17997319-1',
+    'commit': 'd9d67ac4',
+}
+
+
+
+
+if 'html_context' in globals():
+    
+    html_context.update(context)
+    
+else:
+    html_context = context
+
+# Add custom RTD extension
+if 'extensions' in globals():
+    # Insert at the beginning because it can interfere
+    # with other extensions.
+    # See https://github.com/rtfd/readthedocs.org/pull/4054
+    extensions.insert(0, "readthedocs_ext.readthedocs")
+else:
+    extensions = ["readthedocs_ext.readthedocs"]
+
+# Build the json artifacts with the html build step
+rtd_generate_json_artifacts = False
